d1c36287be663385147f2b3c02029672e40ac633
ivansey <van.secret2015@gmail.com>
2019-09-12 20:15:04 +0300

    Add company info in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,8 @@ class Footer extends React.Component {
 		return <div className="footer">
 			<div>
 				<p>Информация об организации</p>
+				<p>ОГРНИП: 309366807100033</p>
+				<p>ИНН: 570304030654</p>
 				<br/>
 				<p>(c) BDV - 2019</p>
 			</div>
